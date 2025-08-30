6322341ba8e78396081ab19ae8c3500bc1b3a0d8
gmmorales <gustavom.morales@gmail.com>
2025-08-30 15:33:19 -0300

    Desarrollo del ejercicio práctico número dos

---
+++ ejercicioPracticoDos.py
@@ -0,0 +1,9 @@
+# Ejercicio pŕactico número dos
+
+name = input("A continuación ingresé su nombre: ")
+lastName = input("A continuación ingresé su apellido: ")
+age = int(input("A continuación ingresé su edad:"))
+email = input("A continuación ingresé dirección de email: ")
+
+
+print(name + " " + lastName + "\n\n" + str(age) + " años" + "\n\n" + email)
